ffe6b1e7ce0f1d9e37dd659664135f9924dadcea
mager <mager@uber.com>
2024-10-22 15:12:47 -0500

    Update organizations YAML & add decentralized test

--- src/__tests__/organizations/create-organization.test.ts
+++ src/__tests__/organizations/create-organization.test.ts
@@ -1,7 +1,6 @@
 import crypto from 'crypto';
 import { OrganizationsClient, createOrganizationsClient } from '../../organizations';
-import type { BillingType, OnboardingInviteType } from '../../organizations/types';
-import { accessToken, customerId } from '../../__fixtures__';
+import { accessToken, getCreateOrgReq } from "../../__fixtures__";
 import { getHeaders } from '../../utils';
 
 describe('createOrganization', () => {
@@ -16,20 +15,32 @@ describe('createOrganization', () => {
     jest.resetAllMocks();
   });
 
-  it('calls fetchData with the correct arguments', async () => {
-    // TODO: Use fixtures
-    const createOrgReq = {
-      info: {
-        name: 'Test Organization',
-        billing_type: 'BILLING_TYPE_CENTRALIZED' as BillingType,
-      },
-      hierarchy_info: {
-        parent_organization_id: '4fe73ff8-0c9a-5ca3-aa2f-17ef3a8487d5',
-      },
-      options: {
-        onboarding_invite_type: 'ONBOARDING_INVITE_TYPE_EMAIL' as OnboardingInviteType,
-      },
+  it('calls fetchData with the correct arguments - centralized org', async () => {
+    const createOrgReq = getCreateOrgReq();
+    const createOrgResp = {
+      organization_id: crypto.randomUUID(),
     };
+
+    global.fetch = jest.fn().mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValueOnce(createOrgResp),
+    });
+
+    const resp = await organizationsClient.createOrganization(createOrgReq);
+    const method = 'POST';
+    expect(global.fetch).toHaveBeenCalledWith(
+      `${organizationsClient.baseURL}/organizations`,
+      {
+        method,
+        headers: getHeaders(accessToken, method),
+        body: JSON.stringify(createOrgReq),
+      }
+    );
+    expect(resp).toEqual(createOrgResp);
+  });
+
+  it('calls fetchData with the correct arguments - decentralized org', async () => {
+    const createOrgReq = getCreateOrgReq("BILLING_TYPE_DECENTRALIZED");
     const createOrgResp = {
       organization_id: crypto.randomUUID(),
     };
--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -3,68 +3,83 @@
  * Do not make direct changes to the file.
  */
 
-
 export interface paths {
-  "/oauth/v2/token": {
-    /**
-     * Login
-     * @description Get a valid access token.
-     */
-    post: {
-      requestBody: {
-        content: {
-          "application/x-www-form-urlencoded": components["schemas"]["LoginReq"];
+    "/oauth/v2/token": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
         };
-      };
-      responses: {
-        /** @description Successfully fetched an access token */
-        200: {
-          content: {
-            "application/json": components["schemas"]["LoginResp"];
-          };
+        get?: never;
+        put?: never;
+        /**
+         * Login
+         * @description Get a valid access token.
+         */
+        post: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody: {
+                content: {
+                    "application/x-www-form-urlencoded": components["schemas"]["LoginReq"];
+                };
+            };
+            responses: {
+                /** @description Successfully fetched an access token */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "application/json": components["schemas"]["LoginResp"];
+                    };
+                };
+            };
         };
-      };
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-  };
 }
-
 export type webhooks = Record<string, never>;
-
 export interface components {
-  schemas: {
-    LoginReq: {
-      /** @description Your application client ID */
-      client_id?: string;
-      /** @description Your application client secret */
-      client_secret?: string;
-      /** @enum {string} */
-      grant_type?: "client_credentials";
-      /**
-       * @description A space-separated list of scopes
-       * @example eats.deliveries direct.organizations
-       */
-      scope?: string;
+    schemas: {
+        LoginReq: {
+            /** @description Your application client ID */
+            client_id?: string;
+            /** @description Your application client secret */
+            client_secret?: string;
+            /** @enum {string} */
+            grant_type?: "client_credentials";
+            /**
+             * @description A space-separated list of scopes
+             * @example eats.deliveries direct.organizations
+             */
+            scope?: string;
+        };
+        LoginResp: {
+            /** @description Used to make authenticated calls to the API */
+            access_token?: string;
+            /** @description Should be Bearer */
+            token_type?: string;
+            /** @description Number of seconds that the access token is valid */
+            expires_in?: number;
+            /** @description Should match the scopes you passed in */
+            scope?: string;
+        };
     };
-    LoginResp: {
-      /** @description Used to make authenticated calls to the API */
-      access_token?: string;
-      /** @description Should be Bearer */
-      token_type?: string;
-      /** @description Number of seconds that the access token is valid */
-      expires_in?: number;
-      /** @description Should match the scopes you passed in */
-      scope?: string;
-    };
-  };
-  responses: never;
-  parameters: never;
-  requestBodies: never;
-  headers: never;
-  pathItems: never;
+    responses: never;
+    parameters: never;
+    requestBodies: never;
+    headers: never;
+    pathItems: never;
 }
-
 export type $defs = Record<string, never>;
-
-export type external = Record<string, never>;
-
 export type operations = Record<string, never>;
